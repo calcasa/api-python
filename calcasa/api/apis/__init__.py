3f9b1d770319cfdc478c4b11054967255d285add
Erwin de Haan <erwin.de.haan@calcasa.nl>
2022-08-04 11:32:45 +0200

    Release 1.2.0 (2022-08-04)
    - Add support for managing `CallbackSubscription`'s, this allows you to subscribe to callbacks for valuations that were not created with your API client.
        - `GET /v1/callbacks/inschrijvingen`
        - `POST /v1/callbacks/inschrijvingen`
        - `GET /v1/callbacks/inschrijvingen/{bagNummeraanduidingId}`
        - `DELETE /v1/callbacks/inschrijvingen/{bagNummeraanduidingId}`
    - Add `taxateurnaam` field to the `Taxatiedata` model.
    - Callback URIs should now end in `/` not just contain it to help stop common errors (ending in `=` is also still allowed when using a query string).
    - Updating configuration in the `POST /v1/configuratie/callbacks` endpoint now clears stored but decommissioned versions from the configuration object.
    - Add `klantkenmerk` to the `WaarderingInputParameters` and `Waardering` models.

--- calcasa/api/apis/__init__.py
+++ calcasa/api/apis/__init__.py
@@ -18,6 +18,7 @@ from calcasa.api.api.adressen_api import AdressenApi
 from calcasa.api.api.bestemmingsplannen_api import BestemmingsplannenApi
 from calcasa.api.api.bodem_api import BodemApi
 from calcasa.api.api.buurt_api import BuurtApi
+from calcasa.api.api.callbacks_api import CallbacksApi
 from calcasa.api.api.configuratie_api import ConfiguratieApi
 from calcasa.api.api.facturen_api import FacturenApi
 from calcasa.api.api.fotos_api import FotosApi
